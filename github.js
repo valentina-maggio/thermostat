e8460b61b1bddc0fa4f3559782a7b32ddde4aae4
Valentina <maggio.vale@gmail.com>
2022-03-31 11:13:42 +0100

    Add callback to return value

--- github.js
+++ github.js
@@ -1,6 +1,12 @@
 import got from 'got';
 
-got('https://api.github.com/repos/sinatra/sinatra').then((response) => {
-  const responseObject = JSON.parse(response.body);
-  console.log(responseObject);
-});
+const fetchRepoInfo = (repoName, callback) => {
+  got(`https://api.github.com/repos/${repoName}`).then((response) => {
+    const responseObject = JSON.parse(response.body);
+    callback(responseObject);
+  });
+}
+
+fetchRepoInfo('sinatra/sinatra', (repoResponse) => {
+  console.log(repoResponse);
+}); 
